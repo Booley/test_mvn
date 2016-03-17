3b8a5f4b5d017deaa4e9c77b038086717224efa5
Bo Moon <moonbh10@gmail.com>
2016-03-17 12:06:18 +0000

    simple program with mat

---
+++ src/main/java/jar/HelloWorld.java
@@ -0,0 +1,13 @@
+package jar;
+
+import org.bytedeco.javacpp.opencv_core.Mat;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hey");
+		Mat m = new Mat();
+		System.out.println(m);
+	}
+}
